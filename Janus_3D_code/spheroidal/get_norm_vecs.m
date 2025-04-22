78c30bcc9fe19d5b03ca3b67dffd60b71aca0b2f
Brian Ha <121746683+real-brian-ha@users.noreply.github.com>
2025-04-22 13:20:21 -0600

    Added get_norm_vecs.m, renamed test folder for L2Stk, and moved spectra test files into its own folder.

---
+++ Janus_3D_code/spheroidal/get_norm_vecs.m
@@ -0,0 +1,21 @@
+%{
+    Extracts the normal vector given p and u0 for a spheroid.
+
+    Note that this is from SpheroidalParameters.m
+%}
+
+
+function nvecs = get_norm_vecs(p, u0, oblate_flag)
+    np = 2*p*(p+1);
+    [theta_x, phi_x] = gl_grid(p);
+    v_x=cos(theta_x);
+    if ~oblate_flag
+        nvecs = 1./sqrt((u0^2-1).*(u0^2-v_x.^2)).*[u0.*sqrt(u0.^2-1).*sqrt(1-v_x.^2).*cos(phi_x),...
+            u0.*sqrt(u0^2-1).*sqrt(1-v_x.^2).*sin(phi_x),...
+            (u0^2-1).*v_x];
+    else
+        nvecs = 1./sqrt((u0^2+1).*(u0^2+v_x.^2)).*[u0.*sqrt(u0.^2+1).*sqrt(1-v_x.^2).*cos(phi_x),...
+            u0.*sqrt(u0^2+1).*sqrt(1-v_x.^2).*sin(phi_x),...
+            (u0^2+1).*v_x];
+    end
+end
