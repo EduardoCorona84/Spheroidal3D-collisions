428e8e3e7679bb981172f99cb14bcf34bb85b549
Nic Rummel <nicrummel@gmail.com>
2025-09-29 07:55:54 -0600

    Test speed up

--- Janus_3D_code/LCPsolvers/test/matVecFromFile.m
+++ Janus_3D_code/LCPsolvers/test/matVecFromFile.m
@@ -17,7 +17,7 @@ addpath(genpath(fullfile(basedir, 'LCPsolvers/solvers')))
 addpath(fullfile(basedir, 'FMMLIB/fmmlib3d-1.2/matlab'));
 addpath(fullfile(basedir,'FMMLIB/stfmmlib3d-1.2/matlab'));
 
-load(fullfile(dirname, '../data/amphiLCPs.n_5.p_8.cDist_2.3.prt_0.mat'), 'Fparams', 'lcp_list');
+load(fullfile(dirname, '../data/amphiLCPs.n_2.p_8.cDist_2.3.prt_0.mat'), 'Fparams', 'lcp_list');
 mc = 2;
 F = lcp_list(mc).F;
 nc = size(F,2);
@@ -25,8 +25,20 @@ C = lcp_list(mc).C;
 p = 8;
 A = getMatVec(Fparams, F, C, p);
 disp(['Mat Vec for ix ' num2str(mc)])
-disp('run time ')
-tic
-A(ones(nc,1));
-toc
-disp(' seconds')
+% disp('run time ')
+%%
+rng('default')
+rng(1)
+np =10;
+out = cell(np,1);
+for n = 1:np
+    disp(['n = ' num2str(n)])
+    v = rand(nc, 1);
+    tic
+    u = A(ones(nc,1));
+    toc
+    % disp(' seconds')
+    out{n} = u;
+end
+% save('OldOut.mat', 'out')
+save('FastOut.mat', 'out')
