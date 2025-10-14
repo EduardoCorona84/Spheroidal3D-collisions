946b5e4b04718baafd85cb56112523fe8d78e0e2
Nic Rummel <nicrummel@gmail.com>
2025-10-14 07:51:21 -0600

    started to make a plotting script

--- Janus_3D_code/plot_spheres.m
+++ Janus_3D_code/plot_spheres.m
@@ -1,18 +1,31 @@
-clear;clc;
-load('BIE_TEST_DATA');
-
-
+% clear;clc;
+% load('BIE_TEST_DATA');
+set(0,'DefaultFigureWindowStyle','docked')
 figure
-hold on
-for i=1:size(r)
-    
-    [Sx, Sy, Sz]=sphere(40);
-    Sx=r(i)*Sx;
-    Sy=r(i)*Sy;
-    Sz=r(i)*Sz;
-    Sc=C(i,:);
-    mesh(Sx+Sc(1),Sy+Sc(2),Sz+Sc(3))%,'edgecolor', 'k');
-    
+ax = axes;
+ax.FontSize = 16;
+grid on
+view(50,1)
+axis vis3d
+% axis tight manual
+
+grp = hgtransform(Parent=ax);
+n3 = size(Ct{1},1);
+nt = numel(Ct);
+if ~exist('r','var')
+    r = ones(n3,1);
 end
-view(50,10);
-ax=gca; ax.FontSize = 16;
+
+for i = 1:nt
+    C = Ct{i};
+    for k=1:n3
+        [Sx, Sy, Sz]=sphere(40);
+        Sx=r(k)*Sx;
+        Sy=r(k)*Sy;
+        Sz=r(k)*Sz;
+        Sc=C(k,:);
+        surf(Sx+Sc(1),Sy+Sc(2),Sz+Sc(3),Parent=grp,EdgeColor='k');
+    end
+    drawnow
+end
+
