f9694e7b652fa12365d705a3a9e0acff1cb6cef4
tyler <52113927+tjensengit@users.noreply.github.com>
2025-10-21 13:34:58 -0600

    Added a few methods/tests

--- Janus_3D_code/LCPsolvers/solvers/accelerated_prox_grad.m
+++ Janus_3D_code/LCPsolvers/solvers/accelerated_prox_grad.m
@@ -23,8 +23,7 @@ function [x, info] = accelerated_prox_grad(fg, x0, opts)
     end
     s = [];
     y = [];
-    f_k = (1/2)*x_k'*Ax_k + opts.b'*x_k;
-    grad_k = Ax_k + opts.b;
+    [f_k, grad_k] = fg(x_k, Ax_k);
     k = 0;
     alpha_k = 1;
     while true
@@ -48,35 +47,22 @@ function [x, info] = accelerated_prox_grad(fg, x0, opts)
         x_k = back_track(fg, x_k, x_km1, kappa, p, opts);
         % Possibly a step length update after the projection
         q = x_k - x_km1;
-        if strcmp (opts.stepSize.eta, 'opt')
-            [eta, Aq] = stepSize(-1, q, x_km1, Ax_km1, opts);
-            Ax_k = Ax_km1 + eta*Aq;
-            x_k = x_km1 + eta*q;
-        else
-            %do nothing
-        end
+        [eta, Aq] = stepSize(-1, q, x_km1, Ax_km1, opts);
+        Ax_k = Ax_km1 + eta*Aq;
+        x_k = x_km1 + eta*q;
+        %compute f_k
+        f_k = (1/2)*x_k'*Ax_k + opts.b'*x_k;
         %acceleration step
         alpha_km1 = alpha_k;
         alpha_k = 1 + sqrt(1 + 4*alpha_km1^2)/2;
         %intermediate acceleration step
         z_k = x_k + (alpha_km1 - 1)/alpha_k * (x_k - x_km1);
-        curr_weight = (alpha_km1 - 1)/alpha_k;
+        Az_k = Ax_k + (alpha_km1 - 1)/alpha_k * (Ax_k - Ax_km1);
         if opts.acceleration.restart && dot(z_k - x_k, x_k - x_km1) > 0
             alpha_k = 1;
         end
-        %compute f_k
-        f_k = (1/2)*x_k'*Ax_k + opts.b'*x_k;
-
         %compute grad_k
-        if strcmp(opts.stepSize.eta, 'opt')
-            %We can reuse the old gradient computations if we are clever
-            %we can write z_k = (1 + curr_weight)*x_k - curr_weight*x_km1
-            %so A(z_k) = (1 + curr_weight)*A(x_k) - curr_weight*A(x_km1)
-            grad_k = (1 + curr_weight)*Ax_k - curr_weight*Ax_km1 + opts.b;
-        else
-            grad_k = opts.A(z_k) + opts.b;
-        end
-        %A more permanent/elegant solution to this is desirable
+        [~, grad_k] = fg(z_k, Az_k);
         s = x_k - x_km1;
         y = grad_k - grad_km1;
     end
--- Janus_3D_code/LCPsolvers/test/test_prox_grad.m
+++ Janus_3D_code/LCPsolvers/test/test_prox_grad.m
@@ -2,10 +2,40 @@ addpath('../inexact-or-multifidelity/utilities/');
 addpath('../solvers/');
 n = 100;
 matrices = construct_test_matrices(n);
+Amat = matrices.linear50.matrix;
 b = 2*(randn(n, 1) - 1/2);
 
-fg = create_fg(matrices.linear50.matrix, b);
+fg = LOCAL_create_fg(Amat, b);
+opts.storeIts = true;
 opts.solver = 'apgd';
 opts.b = b;
-opts.A = @(x) matrices.linear50.matrix * x;
+opts.A = @(x) Amat * x;
 [x, info] = accelerated_prox_grad(fg, zeros(n, 1), opts);
+
+cvx begin
+    variable x_cvx(n)
+    minimize( (1/2)*quad_form(x_cvx, Amat) + dot(x_cvx, b))
+    subject to
+        x_cvx >= 0
+cvx end
+
+
+
+function fg = LOCAL_create_fg(A_init,b)
+    %this function returns a function handle fg that fits into nic's code/template given a matrix evluation A and a vector b.
+    if ~isa(A_init, 'function_handle')
+        A = @(x) A_init*x;
+    end
+    fg = @temp_fg;
+    function [f_k, grad_k] = temp_fg(x_k, Ax_k)
+
+        %If we don't have a previous evaluation, compute it
+        if nargin < 2 || isempty(Ax_k) 
+            Ax_k = A(x_k);
+        end
+
+        grad_k = Ax_k + b;
+        f_k = 0.5*x_k'*Ax_k + b'*x_k;
+    end
+
+end
