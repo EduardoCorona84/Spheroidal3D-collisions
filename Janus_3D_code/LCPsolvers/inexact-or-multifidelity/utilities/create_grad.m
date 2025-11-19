d140e6a02b7027e86b15dadf618eafc728777d0e
tyler <52113927+tjensengit@users.noreply.github.com>
2025-11-18 17:06:10 -0700

    Deleted my multifidelity and inexact code folder as it had duplicate function declarations and the code that I am using now I have written into LCPsolvers/solvers

--- Janus_3D_code/LCPsolvers/inexact-or-multifidelity/utilities/create_grad.m
+++
@@ -1,8 +0,0 @@
-function grad = create_grad(fg)
-    %this function undoes create_fg and only gives back the gradient (primarily useful for the inner solver)
-    grad = @temp_grad;
-    function g = temp_grad(x_k)
-        [~, g, ~] = fg(x_k, [], [], []);
-
-    end
-end
